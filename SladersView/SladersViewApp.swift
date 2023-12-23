6459e93241f38706b18ef510632ec1c839a257f8
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-12-23 16:22:14 +0500

    Initial Commit

---
+++ SladersView/SladersViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SladersViewApp.swift
+//  SladersView
+//
+//  Created by Лилия Андреева on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SladersViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
